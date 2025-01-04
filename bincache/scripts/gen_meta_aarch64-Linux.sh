6df58d8c50b55f2ba9815dffba5b3ca286b96286
Azathothas <AjamX101@gmail.com>
2025-01-05 00:03:00 +0545

    sanity

--- bincache/scripts/gen_meta_aarch64-Linux.sh
+++ bincache/scripts/gen_meta_aarch64-Linux.sh
@@ -242,7 +242,7 @@ jq '
   (map(select(.download_count != -1)) | sort_by(.rank, .pkg)) as $valid_entries |
   (map(select(.download_count == -1)) | sort_by(.pkg)) as $invalid_entries |
   ($valid_entries + $invalid_entries) | to_entries | map(.value.rank = (.key + 1 | tostring)) |
-  map(.value) | sort_by(.pkg)' | jq '.[] | .download_count |= tostring' > "${TMPDIR}/bincache_aarch64-Linux.json"
+  map(.value) | sort_by(.pkg)' | jq '.[] | .download_count |= tostring' | jq 'if type == "array" then . else [.] end' > "${TMPDIR}/bincache_aarch64-Linux.json"
 #sanity check: jq 'sort_by(.rank | tonumber) | map({pkg_name, rank, download_count})'
 ##Check
 unset PKG_COUNT; PKG_COUNT="$(jq -r '.[] | .ghcr_pkg' "${TMPDIR}/bincache_aarch64-Linux.json" | sort -u | wc -l | tr -d '[:space:]')"

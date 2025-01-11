7450302742fcecc31bfffc432eaae1770364109c
Ajam <AjamX101@gmail.com>
2025-01-11 21:14:29 +0545

    better

--- soarpkgs/scripts/gen_diff.sh
+++ soarpkgs/scripts/gen_diff.sh
@@ -11,7 +11,7 @@ if [ -d "${GITHUB_WORKSPACE}" ] && [ "$(find "${GITHUB_WORKSPACE}" -mindepth 1 -
     rebuild: false,
     pkg_family: .pkg_family,
     description: (if (.description | type) == "object" then .description._default else .description end),
-    ghcr_pkg: ("ghcr.io/pkgforge/bincache/" + .pkg + "/" + (.build_script | split(".")[-2])), 
+    ghcr_pkg: ("ghcr.io/pkgforge/bincache/" + .pkg + "/" + (.build_script | split(".")[-3])), 
     build_script: .build_script
   }]' > "${GITHUB_WORKSPACE}/main/soarpkgs/data/DIFF_bincache.json"
   #Pkgcache
@@ -20,7 +20,7 @@ if [ -d "${GITHUB_WORKSPACE}" ] && [ "$(find "${GITHUB_WORKSPACE}" -mindepth 1 -
     rebuild: false,
     pkg_family: .pkg_family,
     description: .description,
-    ghcr_pkg: ("ghcr.io/pkgforge/pkgcache/" + .pkg + "/" + .pkg_type + "/" + .pkg_family + "/" + (.build_script | split(".")[-2])), 
+    ghcr_pkg: ("ghcr.io/pkgforge/pkgcache/" + .pkg + "/" + .pkg_type + "/" + (.build_script | split(".")[-3]) + "/" + (.build_script | split(".")[-2])),
     build_script: .build_script
   }]' > "${GITHUB_WORKSPACE}/main/soarpkgs/data/DIFF_pkgcache.json"
 fi

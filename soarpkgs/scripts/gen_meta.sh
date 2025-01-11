21bf0088f9acba1d137d6fe7015bee745227da6a
Ajam <AjamX101@gmail.com>
2025-01-11 20:59:22 +0545

    Update gen_meta.sh

--- soarpkgs/scripts/gen_meta.sh
+++ soarpkgs/scripts/gen_meta.sh
@@ -423,22 +423,6 @@ fi
 
 #-------------------------------------------------------#
 ##Generate Diff
-#Bincache
-curl -qfsSL "https://meta.pkgforge.dev/soarpkgs/INDEX.json" | jq '[.[] | select((.build_script | contains("https://github.com/pkgforge/soarpkgs/blob/main/binaries")) and (.bincache | not)) | {
-  _disabled: (.["_disabled"] | test("false") | not),
-  rebuild: false,
-  pkg_family: .pkg_family,
-  description: (if (.description | type) == "object" then .description._default else .description end),
-  ghcr_pkg: ("ghcr.io/pkgforge/bincache/" + .pkg + "/" + .pkg_type + "/" + .pkg_family + "/" + (.build_script | split(".")[-2])), 
-  build_script: .build_script
-}]' > "${GITHUB_WORKSPACE}/main/soarpkgs/data/DIFF_bincache.json"
-#Pkgcache
-curl -qfsSL "https://meta.pkgforge.dev/soarpkgs/INDEX.json" | jq '[.[] | select((.build_script | contains("https://github.com/pkgforge/soarpkgs/blob/main/packages")) and (.pkgcache | not)) | {
-  _disabled: (.["_disabled"] | test("false") | not),
-  rebuild: false,
-  pkg_family: .pkg_family,
-  description: .description,
-  ghcr_pkg: ("ghcr.io/pkgforge/pkgcache/" + .pkg + "/" + .pkg_type + "/" + .pkg_family + "/" + (.build_script | split(".")[-2])), 
-  build_script: .build_script
-}]' > "${GITHUB_WORKSPACE}/main/soarpkgs/data/DIFF_pkgcache.json"
+cd "${GITHUB_WORKSPACE}/main" && git pull origin main --no-edit 2>/dev/null
+bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/metadata/refs/heads/main/soarpkgs/scripts/gen_diff.sh")
 #-------------------------------------------------------#

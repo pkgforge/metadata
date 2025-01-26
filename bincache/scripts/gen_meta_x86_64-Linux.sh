b1520062a589e9d027c0023959f42862b7b78853
Ajam <AjamX101@gmail.com>
2025-01-26 16:00:06 +0545

    fix

--- bincache/scripts/gen_meta_x86_64-Linux.sh
+++ bincache/scripts/gen_meta_x86_64-Linux.sh
@@ -290,7 +290,7 @@ jq '
   (map(select(.download_count != -1)) | sort_by(.rank, .pkg)) as $valid_entries |
   (map(select(.download_count == -1)) | sort_by(.pkg)) as $invalid_entries |
   ($valid_entries + $invalid_entries) | to_entries | map(.value.rank = (.key + 1 | tostring)) |
-  map(.value) | sort_by(.pkg)' | jq '.[] | .download_count |= tostring' | jq 'walk(if type == "boolean" then tostring else . end)' | jq -s 'if type == "array" then . else [.] end' | jq -s 'if type == "array" then . else [.] end' | jq 'unique_by(.ghcr_pkg) | sort_by(.pkg)' > "${TMPDIR}/bincache_x86_64-Linux.json"
+  map(.value) | sort_by(.pkg)' | jq '.[] | .download_count |= tostring' | jq 'walk(if type == "boolean" then tostring else . end)' | jq -s 'if type == "array" then . else [.] end' | jq 'unique_by(.ghcr_pkg) | sort_by(.pkg)' > "${TMPDIR}/bincache_x86_64-Linux.json"
 #sanity check rank : jq 'sort_by(.rank | tonumber) | map({pkg_name, rank, download_count})'
 #sanity check urls
 sed -E 's~\bhttps?:/{1,2}\b~https://~g' -i "${TMPDIR}/bincache_x86_64-Linux.json"
